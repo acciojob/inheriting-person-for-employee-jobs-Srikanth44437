92ea2f9439d49452cba658ffbe9324f21b0d9a48
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-20 12:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-function Person(name, age){
-  this.name = name;
-  this.age = age;
+function Person(name, age) {
+	this.name=name;
+	this.age=age;
+}
+Person.prototype.greet = function(){
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
-Person.prototype.greet = function()
-{
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-}
-
-
-
-function Employee(name, age, jobTitle)
-{
-  Person.call(this, name, age);
-  this.jobTitle = jobTitle;
+function Employee(name, age, jobTitle) {
+	Person.call(this,name,age);
+	this.jobTitle=jobTitle;
 }
 
 Employee.prototype = Object.create(Person.prototype);
 
-Employee.prototype.jobGreet= function()
-{
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+Employee.prototype.jobGreet = function(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
 
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
-
-
