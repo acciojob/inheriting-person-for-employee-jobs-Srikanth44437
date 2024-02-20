b14dcae37d2be2de9ca3b2f7db3bbfbd2f25be60
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-20 12:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function Employee(name, age, jobTitle)
   this.jobTitle = jobTitle;
 }
 
-Employee.prototype. = Object.create(Person.prototype);
+Employee.prototype = Object.create(Person.prototype);
 
 Employee.prototype.jobGreet= function()
 {
