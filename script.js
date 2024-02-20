94f57485323aad2a28775b804384d3e0b64a83e6
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-20 12:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-class Person(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() 
-  {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+function Person(name, age){
+  this.name = name;
+  this.age = age;
 }
 
-class Employee(name, age, jobTitle){
-	Person.call(name, age);
-    this.jobTitle = jobTitle;
+Person.prototype.greet = function()
+{
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+}
+
+
+
+function Employee(name, age, jobTitle)
+{
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
 }
 
 Employee.prototype.__proto__ = Person.prototype;
 
-Employee.prototype.jobGreet()
+Employee.prototype.jobGreet= function()
 {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 }
+
+
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
 
 
 // Do not change code below this line
